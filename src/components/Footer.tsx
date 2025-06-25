2bbb2054f0b3870dcaac36377d2200c77eff3d27
teamtails <beckett.bonness@gmail.com>
2025-06-25 13:51:25 -0400

    footer colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,32 +18,32 @@ export const Footer = () => {
               Connecting passionate volunteers to create positive change in animal welfare communities nationwide.
             </p>
             <div className="flex space-x-4 mt-6">
-              <Facebook className="w-6 h-6 text-gray-400 hover:text-blue-400 cursor-pointer transition-colors duration-300" />
-              <Twitter className="w-6 h-6 text-gray-400 hover:text-blue-400 cursor-pointer transition-colors duration-300" />
-              <Instagram className="w-6 h-6 text-gray-400 hover:text-pink-400 cursor-pointer transition-colors duration-300" />
+              <Facebook className="w-6 h-6 text-white hover:text-blue-400 cursor-pointer transition-colors duration-300" />
+              <Twitter className="w-6 h-6 text-white hover:text-blue-400 cursor-pointer transition-colors duration-300" />
+              <Instagram className="w-6 h-6 text-white hover:text-pink-400 cursor-pointer transition-colors duration-300" />
             </div>
           </div>
           
           <div>
             <h4 className="text-lg font-semibold mb-4">Get Involved</h4>
-            <ul className="space-y-2 text-gray-400">
-              <li><a href="#" className="hover:text-white transition-colors duration-300">Join a Chapter</a></li>
-              <li><a href="/chapters" className="hover:text-white transition-colors duration-300">Find Your Chapter</a></li>
-              <li><a href="#" className="hover:text-white transition-colors duration-300">Start a Chapter</a></li>
+            <ul className="space-y-2 text-white">
+              <li><a href="#" className="hover:text-gray-400 transition-colors duration-300">Join a Chapter</a></li>
+              <li><a href="/chapters" className="hover:text-gray-400 transition-colors duration-300">Find Your Chapter</a></li>
+              <li><a href="#" className="hover:text-gray-400 transition-colors duration-300">Start a Chapter</a></li>
             </ul>
           </div>
           
           <div>
             <h4 className="text-lg font-semibold mb-4">About Us</h4>
-            <ul className="space-y-2 text-gray-400">
-              <li><a href="/about" className="hover:text-white transition-colors duration-300">Our Mission</a></li>
-              <li><a href="/officers" className="hover:text-white transition-colors duration-300">Meet Our Officers</a></li>
+            <ul className="space-y-2 text-white">
+              <li><a href="/about" className="hover:text-gray-400 transition-colors duration-300">Our Mission</a></li>
+              <li><a href="/officers" className="hover:text-gray-400 transition-colors duration-300">Meet Our Officers</a></li>
             </ul>
           </div>
           
           <div>
             <h4 className="text-lg font-semibold mb-4">Contact</h4>
-            <ul className="space-y-2 text-gray-400">
+            <ul className="space-y-2 text-white">
               <li>123 Volunteer Way</li>
               <li>Washington, DC 20001</li>
               <li>(555) 123-TAILS</li>
