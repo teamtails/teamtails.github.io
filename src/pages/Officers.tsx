3590ec4d46fe650ff023e28f52d4abf8354df206
teamtails <beckett.bonness@gmail.com>
2025-06-25 13:21:28 -0400

    gfgfg

--- src/pages/Officers.tsx
+++ src/pages/Officers.tsx
@@ -64,7 +64,7 @@ const Officers = () => {
         }}
       >
         {/* Dark overlay for better text readability */}
-        <div className="absolute inset-0 bg-black/50"></div>
+        <div className="absolute inset-0 bg-black/5"></div>
         
         <div className="container mx-auto px-6 text-center relative z-10">
           <h1 className="text-4xl md:text-6xl font-bold mb-6">
