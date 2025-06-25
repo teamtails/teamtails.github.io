e05ed3d9da8ecf620502ad074c83cd9bc2e4f68c
teamtails <beckett.bonness@gmail.com>
2025-06-25 13:45:04 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ import { Heart, Facebook, Twitter, Instagram } from "lucide-react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-12">
+    <footer 
+      className="text-white py-12 bg-cover bg-center relative"
+      style={{ backgroundImage: `url("/background.png")` }}
+    >
       <div className="container mx-auto px-6">
         <div className="grid md:grid-cols-4 gap-8">
           <div className="col-span-2 md:col-span-1">
