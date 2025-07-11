00ce94c32ea4fb0c5e8c30a82f65c78c210070df
teamtails <beckett.bonness@gmail.com>
2025-07-11 09:53:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
+import Events from "./pages/Events";
 import Officers from "./pages/Officers";
 import Chapters from "./pages/Chapters";
 import NotFound from "./pages/NotFound";
