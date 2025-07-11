829db1dd6e88076c77d3a67489b93f1159333c80
teamtails <beckett.bonness@gmail.com>
2025-07-11 09:52:40 -0400

    Update Events.tsx

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,11 +1,6 @@
 
 import { Navigation } from "@/components/Navigation";
-import { Hero } from "@/components/Hero";
-import { About } from "@/components/About";
-import { Impact } from "@/components/Impact";
-import { GetInvolved } from "@/components/GetInvolved";
 import { UpcomingEvents } from "@/components/UpcomingEvents";
-import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 
 const Events = () => {
