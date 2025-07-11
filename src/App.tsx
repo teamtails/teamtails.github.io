d7a457d73e35877cdeeb9fbd4b087c61eaf53b09
teamtails <beckett.bonness@gmail.com>
2025-07-11 09:50:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/officers" element={<Officers />} />
           <Route path="/chapters" element={<Chapters />} />
+          <Route path="/events" element={<Events />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
