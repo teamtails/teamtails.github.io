80e7c3c27a7d160dccd4006d1aa35f33df091671
teamtails <beckett.bonness@gmail.com>
2025-07-11 09:43:32 -0400

    a

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,10 +6,15 @@ import { Footer } from "@/components/Footer";
 
 const About = () => {
   return (
-    <Navigation />
-      <AboutSection />
-        <Impact />
-    <Footer />
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-green-50">
+      <Navigation />
+      <hr className="border-white" />
+      <About />
+      <hr className="border-white" />
+      <Impact />
+      <hr className="border-white" />
+      <Footer />
+    </div>
   );
 };
 
