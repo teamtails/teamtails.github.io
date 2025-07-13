e9f8465803a8ccd8ffef77e7ffe02f0a149016c6
teamtails <beckett.bonness@gmail.com>
2025-07-12 21:37:03 -0400

    d

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,7 +18,7 @@ export const Navigation = () => {
           </Link>
 
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="hidden md:flex items-center space-x-8">
             <Link to="/" className="text-gray-700 hover:text-blue-600 transition-colors duration-300">
               Home
             </Link>
