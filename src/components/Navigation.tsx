1d7661641fb9ac550a45e838bb868a9ed843bcce
teamtails <beckett.bonness@gmail.com>
2025-07-13 19:59:05 -0400

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,35 +57,33 @@ export const Navigation = () => {
                 Home
               </Link>
               <Link 
-                to="/about" 
-                className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                About Us
-              </Link>
-              <Link 
                 to="/chapters" 
                 className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
                 onClick={() => setIsMenuOpen(false)}
               >
-                Find Your Chapter
+                Chapters
               </Link>
               <Link 
                 to="/officers" 
                 className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
                 onClick={() => setIsMenuOpen(false)}
               >
-                Meet Our Officers
+                Meet our Officers
               </Link>
-              <Button 
-                className="bg-blue-600 hover:bg-blue-700 text-white w-fit"
-                onClick={() => {
-                  setIsMenuOpen(false);
-                  document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' });
-                }}
+              <Link 
+                to="/events" 
+                className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
               >
-                Join Our Mission
-              </Button>
+                Events
+              </Link>
+              <Link 
+                to="/remembrance" 
+                className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Remembrance
+              </Link>
             </div>
           </div>
         )}
