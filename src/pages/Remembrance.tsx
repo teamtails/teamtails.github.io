b3f5bb828bc2465d45a0e21a14fd1b39ce00f27e
teamtails <beckett.bonness@gmail.com>
2025-07-12 21:30:30 -0400

    Update Remembrance.tsx

--- src/pages/Remembrance.tsx
+++ src/pages/Remembrance.tsx
@@ -1,24 +1,13 @@
 
 import { Navigation } from "@/components/Navigation";
-import { Hero } from "@/components/Hero";
-import { About } from "@/components/About";
-import { Impact } from "@/components/Impact";
-import { GetInvolved } from "@/components/GetInvolved";
-import { UpcomingEvents } from "@/components/UpcomingEvents";
-import { Contact } from "@/components/Contact";
+import { Papers } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 
 const Remembrance = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-green-50">
       <Navigation />
-      <Hero />
-      <hr className="border-white" />
-      <About />
-      <hr className="border-white" />
-      <Impact />
-      <hr className="border-white" />
-      <GetInvolved />
+      <Papers />
       <hr className="border-white" />
       <Footer />
     </div>
