ca86a20f4c9460ee2b54dccc7f763174d40c4949
teamtails <beckett.bonness@gmail.com>
2025-07-13 12:08:31 -0400

    Update Papers.tsx

--- src/components/Papers.tsx
+++ src/components/Papers.tsx
@@ -12,9 +12,9 @@ export const Papers = () => {
       action: "Read More",
       color: "bg-blue-50 border-blue-200 hover:bg-blue-100",
       images: [
-        "../../../emmitt/1.jpeg",
-        "../../../emmitt/2.jpeg",
-        "../../../emmitt/3.jpeg"
+        "../../../public/emmitt/1.jpeg",
+        "../../../public/emmitt/2.jpeg",
+        "../../../public/emmitt/3.jpeg"
       ]
     }
   ];
