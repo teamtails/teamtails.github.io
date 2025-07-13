c8b574f89b3ba6d962dbedc97d5635bfd7810e73
teamtails <beckett.bonness@gmail.com>
2025-07-12 22:03:34 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 
 import { Navigation } from "@/components/Navigation";
 import { Hero } from "@/components/Hero";
-import { About } from "@/components/About";
+import { AboutPage } from "@/components/About";
 import { Impact } from "@/components/Impact";
 import { GetInvolved } from "@/components/GetInvolved";
 import { UpcomingEvents } from "@/components/UpcomingEvents";
@@ -14,7 +14,7 @@ const Index = () => {
       <Navigation />
       <Hero />
       <hr className="border-white" />
-      <About />
+      <AboutPage />
       <hr className="border-white" />
       <Impact />
       <hr className="border-white" />
