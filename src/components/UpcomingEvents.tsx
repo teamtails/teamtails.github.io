758be5794447039787571a61d1f787971f387447
teamtails <beckett.bonness@gmail.com>
2025-07-19 15:15:29 -0400

    Update UpcomingEvents.tsx

--- src/components/UpcomingEvents.tsx
+++ src/components/UpcomingEvents.tsx
@@ -5,25 +5,25 @@ import { Calendar, MapPin, Clock, Users } from "lucide-react";
 
 export const UpcomingEvents = () => {
   const events = [
-    {
-      title: "Birdhouse Design Planning",
-      date: "July 17th, 2025",
-      time: "5:00 - 6:00 PM",
-      location: "Zoom",
-      chapter: "All Chapters",
-      description: "Help plan our upcoming birdhouse making event!",
-      spots: "Volunteers needed",
-      link: "https://docs.google.com/forms/d/e/1FAIpQLSfxgPfbQjE6uQcwcpoCmPLG4_4-IvNKFhuws-8TloarWRFxvg/viewform?usp=dialog"
-    },
-    {
+      {
       title: "Social Media Project: Meeting 2",
-      date: "July 18th, 2025",
-      time: "5:00 - 6:00 PM",
+      date: "July 23th, 2025",
+      time: "5:30 PM",
       location: "Zoom",
       chapter: "All Chapters",
       description: "Help create Instagram Reels and TikToks to spread our message!",
       spots: "Volunteers needed",
       link: "https://docs.google.com/forms/d/e/1FAIpQLSf2kwUDJGCqgGW9ha7aPOKUPr2w9yBqXiH4ePEWLGdhB1aYOA/viewform?usp=dialog"
+    },
+    {
+      title: "Birdhouse Design Planning",
+      date: "July 24th, 2025",
+      time: "6:00 PM",
+      location: "Zoom",
+      chapter: "All Chapters",
+      description: "Help plan our upcoming birdhouse making event!",
+      spots: "Volunteers needed",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSfxgPfbQjE6uQcwcpoCmPLG4_4-IvNKFhuws-8TloarWRFxvg/viewform?usp=dialog"
     }
   ];
 
